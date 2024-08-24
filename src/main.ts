43955ee40475d9c4dcb6ea68313a12fd7b9fecdf
bakemono-san <mousdef34@gmail.com>
2024-08-24 01:34:33 +0000

    added vercel file

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import * as dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 
+dotenv.config();
+// console.log('JWT Secret:', process.env.TOKEN);
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+  app.use(cookieParser());
   await app.listen(3000);
 }
 bootstrap();
