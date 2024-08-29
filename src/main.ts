b5120e3d021ee38a47ed29fc2af07a5864561f29
Bakemono <82776147+Bakemono-san@users.noreply.github.com>
2024-08-29 14:29:30 +0000

    Update main.ts && removed dotenv

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import * as dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
@@ -9,8 +8,6 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import yaml from 'yaml';
 import { writeFileSync } from 'fs';
 
-dotenv.config();
-// console.log('JWT Secret:', process.env.TOKEN);
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useGlobalPipes(new ValidationPipe());
