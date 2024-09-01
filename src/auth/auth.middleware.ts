d2a89b8568da87274b4c69a4c0ea1616be91db72
bakemono-san <mousdef34@gmail.com>
2024-09-01 08:58:58 +0000

    updated the middleware

--- src/auth/auth.middleware.ts
+++ src/auth/auth.middleware.ts
@@ -8,33 +8,13 @@ export class AuthMiddleware implements NestMiddleware {
     private jwt: JwtService,
     private configService: ConfigService,
   ) {}
-
   use(req: any, res: any, next: () => void) {
     const token = req.cookies.token ? req.cookies.token : null;
-
-    // Liste des chemins à exclure de la vérification d'authentification
-    const excludedPaths = [
-      '/swagger-ui.html',
-      '/swagger-ui/',
-      '/v3/api-docs',
-      '/swagger-resources',
-      '/api/swagger-ui-bundle.js',
-      '/api/swagger-ui-standalone-preset.js',
-      '/',
-    ];
-
-    // Vérifiez si le chemin de la requête doit être exclu
-    const isExcluded = excludedPaths.some((path) => req.path === path);
-    console.log(req.path);
-
-    if (isExcluded) {
-      return next();
-    }
-
+    //console.log(token);
     if (!token) {
       return res
         .status(401)
-        .json({ message: 'No token, authorization denied', path: req.path });
+        .json({ message: 'No token, authorization denied' });
     }
 
     try {
